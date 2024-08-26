1927851f0d56d934784588b946bbda713eb1c03e
yeqifeng2288 <39109657+yeqifeng2288@users.noreply.github.com>
2024-08-26 14:45:28 +0800

    Update Add-Packages.sh

--- diy/Add-Packages.sh
+++ diy/Add-Packages.sh
@@ -3,5 +3,7 @@
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 echo 'Load Diy Packages'
 #UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-UPDATE_PACKAGE "homebox" "sirpdboy/netspeedtest" "master" "homebox"
+#UPDATE_PACKAGE "homebox" "sirpdboy/netspeedtest" "master" "homebox"
 #UPDATE_PACKAGE "luci-app-netspeedtest" "sirpdboy/netspeedtest" "master" "luci-app-netspeedtest"
+
+src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git
