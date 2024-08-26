10389ef3b9b6aff69e40265a19b748947cf01ab2
yeqifeng2288 <39109657+yeqifeng2288@users.noreply.github.com>
2024-08-26 23:49:12 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -8,4 +8,4 @@ echo $version
 firmwares_name="immortalwrt.git-main_jdcloud_ax1800-pro-squashfs-sysupgrade_${version}.bin"
 echo $firmwares_name
 wget "https://github.com/yeqifeng2288/OpenWRT-CI/releases/download/IPQ60XX-WIFI-YES_immortalwrt.git-main_$version/immortalwrt.git-main_jdcloud_ax1800-pro-squashfs-sysupgrade_${version}.bin" -O "/tmp/${firmwares_name}"
-sysupgrade -f /tmp/$firmwares_name
+sysupgrade -F /tmp/$firmwares_name
