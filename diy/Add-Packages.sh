95bcced67031296be50f4d2ca4ce088ced37080b
yeqifeng2288 <39109657+yeqifeng2288@users.noreply.github.com>
2024-08-26 18:01:38 +0800

    Update Add-Packages.sh

--- diy/Add-Packages.sh
+++ diy/Add-Packages.sh
@@ -1,26 +1,4 @@
-#安装和更新软件包
-UPDATE_PACKAGE() {
-	local PKG_NAME=$1
-	local PKG_REPO=$2
-	local PKG_BRANCH=$3
-	local PKG_SPECIAL=$4
-	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
-
-	rm -rf $(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune)
-	echo 'cmd git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"'
-	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
-
-	if [[ $PKG_SPECIAL == "pkg" ]]; then
-		cp -rf $(find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune) ./
-		rm -rf ./$REPO_NAME/
-	elif [[ $PKG_SPECIAL == "name" ]]; then
-		mv -f $REPO_NAME $PKG_NAME
-	fi
-}
-
 # Add-Packages.sh
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 echo 'Load Diy Packages'
-#UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-UPDATE_PACKAGE "luci-app-netspeedtest" "sirpdboy/netspeedtest" "master"
-#UPDATE_PACKAGE "luci-app-netspeedtest" "sirpdboy/netspeedtest" "master" "luci-app-netspeedtest"
+#UPDATE_PACKAGE "luci-app-netspeedtest" "sirpdboy/netspeedtest" "master"
