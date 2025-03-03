83f44c03bcbab53648bfe5983d4d2d1fc3e1504a
yeqifeng2288 <yeqifeng2288@live.cn>
2025-03-03 08:55:08 +0800

    同步代码

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -44,13 +44,18 @@ if [ -n "$WRT_PACKAGE" ]; then
 fi
 
 #高通平台调整
+DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
 if [[ $WRT_TARGET == *"QUALCOMMAX"* ]]; then
 	#取消nss相关feed
-	echo "CONFIG_FEED_nss_packages=n" >>./.config
-	echo "CONFIG_FEED_sqm_scripts_nss=n" >>./.config
+	echo "CONFIG_FEED_nss_packages=n" >> ./.config
+	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
 	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >>./.config
-	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >>./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
+	#无WIFI配置调整Q6大小
+	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
+		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\)\.dtsi/ipq\1-nowifi.dtsi/g' {} +
+	fi
 fi
 
 #编译器优化
