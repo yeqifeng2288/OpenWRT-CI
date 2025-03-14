77593ab04df8781cea2829dfa7826eff09fe18b6
yeqifeng2288 <39109657+yeqifeng2288@users.noreply.github.com>
2025-03-15 02:05:11 +0800

    使用默认源的tailscale

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -68,12 +68,12 @@ if [ -f "$SP_FILE" ]; then
 fi
 
 #修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-	sed -i '/\/files/d' $TS_FILE
-
-	cd $PKG_PATH && echo "tailscale has been fixed!"
-fi
+#TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+#if [ -f "$TS_FILE" ]; then
+#	sed -i '/\/files/d' $TS_FILE
+#
+#	cd $PKG_PATH && echo "tailscale has been fixed!"
+#fi
 #修复Coremark编译失败
 CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
 if [ -f "$CM_FILE" ]; then
