17dd6142f89c3fa118ba0a16e9a222cb3d7c4e8e
yeqifeng2288 <39109657+yeqifeng2288@users.noreply.github.com>
2025-05-29 21:20:16 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,10 +2,10 @@
 RELEASES_URL="https://github.com/yeqifeng2288/OpenWRT-CI/tags"
 latest_release_page=$(curl -s $RELEASES_URL)
 
-version=$(echo "$latest_release_page" | awk 'BEGIN{FS="IPQ60XX-WIFI-YES_immortalwrt\.git-main_"; OFS=""}{split($2, a, " "); print a[1]}')
+version=$(echo "$latest_release_page" | awk 'BEGIN{FS="IPQ60XX-WIFI-YES-VIKINGYFY-main-"; OFS=""}{split($2, a, " "); print a[1]}')
 version=$(echo $version | awk -F'["]' '{print $1}')
 echo $version
 firmwares_name="immortalwrt.git-main_qualcommax-ipq60xx-jdcloud_re-ss-01-squashfs-sysupgrade_${version}.bin"
 echo $firmwares_name
-wget "https://github.com/yeqifeng2288/OpenWRT-CI/releases/download/IPQ60XX-WIFI-YES_immortalwrt.git-main_$version/immortalwrt.git-main_qualcommax-ipq60xx-jdcloud_re-ss-01-squashfs-sysupgrade_${version}.bin" -O "/tmp/${firmwares_name}"
+wget "https://github.com/yeqifeng2288/OpenWRT-CI/releases/download/IPQ60XX-WIFI-YES-VIKINGYFY-main-${version}/VIKINGYFY-main-qualcommax
 sysupgrade -F /tmp/$firmwares_name
