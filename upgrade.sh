251336eb7f5f3081c958de7e29951314d2fc5229
yeqifeng2288 <39109657+yeqifeng2288@users.noreply.github.com>
2025-12-25 21:34:31 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -5,7 +5,7 @@ latest_release_page=$(curl -s $RELEASES_URL)
 version=$(echo "$latest_release_page" | awk 'BEGIN{FS="IPQ60XX-WIFI-YES-VIKINGYFY-main-"; OFS=""}{split($2, a, " "); print a[1]}')
 version=$(echo $version | awk -F'["]' '{print $1}')
 echo $version
-firmwares_name="immortalwrt.git-main_qualcommax-ipq60xx-jdcloud_re-ss-01-squashfs-sysupgrade_${version}.bin"
+firmwares_name="immortalwrt.git-main_qualcommax-ipq60xx-jdcloud_re-ss-01-squashfs-sysupgrade-${version}.bin"
 echo $firmwares_name
 wget "https://github.com/yeqifeng2288/OpenWRT-CI/releases/download/IPQ60XX-WIFI-YES-VIKINGYFY-main-${version}/VIKINGYFY-main-qualcommax-ipq60xx-jdcloud_re-ss-01-squashfs-sysupgrade-${version}.bin" -O "/var/tmp/${firmwares_name}"
 mkdir /mnt/mmcblk0p27/backup -p
