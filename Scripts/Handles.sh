615a2005744cbe920b332f0fee886a57fd4728c2
yeqifeng2288 <yeqifeng2288@live.cn>
2026-06-15 13:56:36 +0800

    同步修改

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,27 +2,27 @@
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	echo " "
-
-	HP_RULE="surge"
-	HP_PATH="homeproxy/root/etc/homeproxy"
-
-	rm -rf ./$HP_PATH/resources/*
-
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
-	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
-
-	cd .. && rm -rf ./$HP_RULE/
-
-	cd $PKG_PATH && echo "homeproxy date has been updated!"
-fi
+##预置HomeProxy数据
+#if [ -d *"homeproxy"* ]; then
+#	echo " "
+#
+#	HP_RULE="surge"
+#	HP_PATH="homeproxy/root/etc/homeproxy"
+#
+#	rm -rf ./$HP_PATH/resources/*
+#
+#	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
+#	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+#
+#	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+#	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+#	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+#	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
+#
+#	cd .. && rm -rf ./$HP_RULE/
+#
+#	cd $PKG_PATH && echo "homeproxy date has been updated!"
+#fi
 
 #修改argon主题字体和颜色
 if [ -d *"luci-theme-argon"* ]; then
@@ -49,26 +49,6 @@ if [ -d *"luci-app-mini-diskmanager"* ]; then
 	sed -i "s/services/system/g" ./luci-app-mini-diskmanager/root/usr/share/luci/menu.d/luci-app-mini-diskmanager.json
 
 	cd $PKG_PATH && echo "mini-diskmanager has been fixed!"
-fi
-
-#修改qca-nss-drv启动顺序
-NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-if [ -f "$NSS_DRV" ]; then
-	echo " "
-
-	sed -i 's/START=.*/START=85/g' $NSS_DRV
-
-	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
-fi
-
-#修改qca-nss-pbuf启动顺序
-NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
-if [ -f "$NSS_PBUF" ]; then
-	echo " "
-
-	sed -i 's/START=.*/START=86/g' $NSS_PBUF
-
-	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
 fi
 
 #修复TailScale配置文件冲突
