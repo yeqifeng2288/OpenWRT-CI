3cd0476aa623678bf0802251fe521e44926739b3
yeqifeng2288 <yeqifeng2288@live.cn>
2026-09-20 14:08:26 +0800

    同步

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2026 VIKINGYFY
 
-PKG_PATH="$GITHUB_WORKSPACE/wrt/package"
-
+FEEDS_PATH="./feeds"
+PACKAGE_PATH="./package"
 
 #修改argon主题字体和颜色
-if [ -d "$PKG_PATH/luci-theme-argon" ]; then
+if [ -d "$PACKAGE_PATH/luci-theme-argon" ]; then
 	echo " "
-	if sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" \
-		"$PKG_PATH/luci-theme-argon/luci-app-argon-config/root/etc/config/argon"; then
+	if sed -i "s/primary '.*'/primary '#31a1a1'/g; s/'0.2'/'0.5'/g; s/'none'/'bing'/g; s/'600'/'normal'/g" \
+		"$PACKAGE_PATH/luci-theme-argon/luci-app-argon-config/root/etc/config/argon"; then
 		echo "theme-argon has been fixed!"
 	else
 		echo "theme-argon fix failed; continuing!"
@@ -17,9 +17,9 @@ if [ -d "$PKG_PATH/luci-theme-argon" ]; then
 fi
 
 #修改aurora菜单式样
-if [ -d "$PKG_PATH/luci-app-aurora-config" ]; then
+if [ -d "$PACKAGE_PATH/luci-app-aurora-config" ]; then
 	echo " "
-	if find "$PKG_PATH/luci-app-aurora-config/root/usr/share/aurora/" -type f -name '*.template' -exec \
+	if find "$PACKAGE_PATH/luci-app-aurora-config/root/usr/share/aurora/" -type f -name '*.template' -exec \
 		sed -i "s/nav_type '.*'/nav_type 'dropdown'/g; s/struct_radius_base '.*'/struct_radius_base '0.125rem'/g" {} +; then
 		echo "theme-aurora has been fixed!"
 	else
@@ -28,23 +28,33 @@ if [ -d "$PKG_PATH/luci-app-aurora-config" ]; then
 fi
 
 #修改mini-diskmanager菜单位置
-if [ -d "$PKG_PATH/luci-app-mini-diskmanager" ]; then
+if [ -d "$PACKAGE_PATH/luci-app-mini-diskmanager" ]; then
 	echo " "
 	if sed -i "s/services/system/g" \
-		"$PKG_PATH/luci-app-mini-diskmanager/luci-app-mini-diskmanager/root/usr/share/luci/menu.d/luci-app-mini-diskmanager.json"; then
+		"$PACKAGE_PATH/luci-app-mini-diskmanager/luci-app-mini-diskmanager/root/usr/share/luci/menu.d/luci-app-mini-diskmanager.json"; then
 		echo "mini-diskmanager has been fixed!"
 	else
 		echo "mini-diskmanager fix failed; continuing!"
 	fi
 fi
 
+#修改natmapt菜单位置
+if [ -d "$PACKAGE_PATH/luci-app-natmapt" ]; then
+	echo " "
+	if sed -i "s/network/services/g" \
+		"$PACKAGE_PATH/luci-app-natmapt/root/usr/share/luci/menu.d/luci-app-natmap.json"; then
+		echo "natmapt has been fixed!"
+	else
+		echo "natmapt fix failed; continuing!"
+	fi
+fi
+
 
 #修复Rust编译失败
-RUST_FILE="$(find "$FEEDS_PACKAGES" -maxdepth 3 -type f -wholename '*/rust/Makefile' -print -quit 2>/dev/null)"
-if [ -f "$RUST_FILE" ]; then
+if [ -d "$FEEDS_PATH/packages/lang/rust" ]; then
 	echo " "
-
-	if sed -i 's/ci-llvm=true/ci-llvm=false/g' "$RUST_FILE"; then
+	if sed -i 's/ci-llvm=true/ci-llvm=false/g' \
+		"$FEEDS_PATH/packages/lang/rust/Makefile"; then
 		echo "rust has been fixed!"
 	else
 		echo "rust fix failed; continuing!"
